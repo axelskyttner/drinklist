7568979347c2edfa1304dc161d1c74a7b77f8827
Axel <axelskyttner@gmail.com>
2018-02-12 16:19:52 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+const fs = require("fs")
+var url = require('url');
+let data = require("./data.json")
+
+append_data = (name)=>{
+    data.history.push({name:name, unit:"beer"})
+}
+
+update_data = (username)=>{
+
+    fileName = "data.json"
+    append_data(username)
+    fs.writeFile(fileName, JSON.stringify(data), function (err) {
+      if (err) return console.log(err);
+      console.log(JSON.stringify(data));
+      console.log('writing to ' + fileName);
+    });
+}
+
+
+app.get("/", (req,res)=>res.sendFile(__dirname + "/index.html"))
+app.get('/search', function (req, res) {
+  res.json(data);
+})
+
+app.get('/update_list', function (req, res) {
+    username = req.query.username 
+     
+    update_data(username)
+    res.sendfile(__dirname + "/index.html")
+})
+
+
+app.listen(80, ()=>console.log("listinening"))
